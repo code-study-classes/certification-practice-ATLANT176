9fc0dab3dd7798817e1321ecf9886f2c18b52072
ATLANT176 <danila2726@mail.ru>
2023-11-01 14:19:19 +0300

    String practice

--- strings.js
+++ strings.js
@@ -7,4 +7,21 @@ export function countUppercaseLetters (str) {
     }
     return count;
 }
-  
+
+export function combineStrings (N1, N2, S1, S2) {
+  const firstStringNum = S1.slice(0, N1);
+  const secondStringNum = S2.slice(-N2);
+  if (N1 === 0 && N2 === 0) {
+    return '';
+  }
+  return firstStringNum + secondStringNum;
+}
+
+export function containsSubstring (S, S0) {
+  const str = S.includes(S0);
+  return str;
+}
+
+export function replaceSubstring (S, S1, S2) {
+  return S.replace(S1, S2);
+}
